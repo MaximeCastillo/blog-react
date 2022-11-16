ee8513f89c8ba7502a306b088946ab0d60b7e74b
Maxime Castillo <maxime.castillo@gmail.com>
2022-11-16 11:47:02 +0100

    70. Créer le formulaire

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Form.css'
+
+export default function Form() {
+  return (
+    <>
+      <h1 className="title-form">Écrivez un article</h1>
+
+      <form className="container-form">
+        <label htmlFor="title">Titre</label>
+        <input type="text" id='title' placeholder='Entrez votre nom' />
+
+        <label htmlFor="article">Article</label>
+        <textarea id="article" placeholder='Votre article'></textarea>
+
+        <button>Envoyez l'article</button>
+      </form>
+    </>
+  )
+}
